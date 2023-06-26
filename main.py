d791c790a46e2f05693beace9a61f7ab06bdf283
mehfluffy <alissa.cheng@protonmail.ch>
2023-06-26 13:44:34 +0200

    compare with current largest and add skip clauses (906609, i=404551)

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ def is_palindrome(num: int) -> bool:
     return str(num) == str(num)[::-1]
 
 
+# todo compare speed: this vs itertools
 def generate_factors(pool: tuple) -> Iterator:
-    n = len(pool)
     indices = [0, 0]
-    yield tuple(pool[i] for i in indices)  # this is the first pair
-    # Then, change indices to traverse upper triangle of permutations by column
-    while indices[0] < n:
+    # Mutate indices to traverse upper triangle of permutations by column
+    while indices[1] < len(pool) - 1:
+        yield tuple(pool[i] for i in indices)
         if indices[0] == indices[1]:
-            indices[0] = 0
-            indices[1] += 1
+            indices = [0, indices[1] + 1]  # first row of next column
             yield tuple(pool[i] for i in indices)
         indices[0] += 1
-        yield tuple(pool[i] for i in indices)
 
 
 def find_largest_palindrome():
-    pool = tuple(range(999, 100, -1))
+    pool = tuple(range(999, 99, -1))
+    largest_palindrome = 0
+    largest_factors = (0, 0)
     for factors in generate_factors(pool):
-        if is_palindrome(factors[0] * factors[1]):
-            print(factors[0] * factors[1], factors)
+        product = factors[0] * factors[1]
+        if product <= largest_palindrome:
+            continue
+        # todo does the below check improve speed? esp since store factors
+        if factors[0] < largest_factors[1]:  # needs to be after the above check
             break
+        if is_palindrome(product):
+            largest_palindrome = product
+            largest_factors = factors
+    print(largest_palindrome, largest_factors)
 
 
 if __name__ == '__main__':
